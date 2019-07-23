612fadfd88f847b365323b7c74d0bf11fc7a3ab9
Carles <chervera@gmail.com>
2019-07-23 13:13:49 +0200

    export service, remove file saver and tostring

--- src/app/shared/export/export.service.ts
+++ src/app/shared/export/export.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ExportModule } from './export.module';
-import * as FileSaver from 'file-saver';
 import * as XLSX from 'xlsx';
-import { HttpXsrfTokenExtractor } from '@angular/common/http';
 
 @Injectable({
   providedIn: ExportModule
@@ -25,7 +23,7 @@ export class ExportService {
       let preparedRow = [];
       columnDefinition.forEach(column => {
         if (element[column]) {
-          preparedRow.push(element[column]);
+          preparedRow.push('' + element[column]);
         }
       });
       preparedData.push(preparedRow);
@@ -44,20 +42,12 @@ export class ExportService {
 
     XLSX.writeFile(workbook, this.generateFileName(nameOfTheFile));
 
-    /*const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
-
-    this.saveAsExcelFile(excelBuffer, nameOfTheFile);*/
-
   }
 
   private generateFileName(nameOfTheFile: string): string {
     return nameOfTheFile + this.EXCEL_EXTENSION;
   }
 
-  private saveAsExcelFile(buffer: any, fileName: string): void {
-    const data: Blob = new Blob([buffer], { type: this.EXCEL_TYPE });
-    FileSaver.saveAs(data, fileName + new Date().getTime() + this.EXCEL_EXTENSION);
-  }
 }
 
 export class PreparedToExport {
